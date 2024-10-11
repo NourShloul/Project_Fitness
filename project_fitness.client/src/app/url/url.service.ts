85e7e4a3f76d64d65e278267ae083d2e5ea3b55b
Raneem-Alhalahlah <raneemalhalahleh@gmail.com>
2024-10-11 19:14:30 +0300

    m

--- project_fitness.client/src/app/url/url.service.ts
+++ project_fitness.client/src/app/url/url.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class URLService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  staticData = "https://localhost:7072/api";
+
+
+
+  GetTypeOfRecipe(): Observable<any> {
+    return this.http.get<any>(`${this.staticData}/Recipe/Nutrition/GetAllRescipe`);
+  }
+
+
+  getSubRecipe(id: any): Observable<any> {
+    return this.http.get<any>(`${this.staticData}/Subrecipe/Recipe/GetSubrecipeByRecipeId/${id}`);
+  }
+  getSubRecipeDetails(id: any): Observable<any> {
+    return this.http.get<any>(`${this.staticData}/Subrecipe/Recipe/GetSubrecipeById/${id}`);
+  }
 }
