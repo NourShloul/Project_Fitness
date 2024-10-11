26f3c74f0548a29b9e0aa28409e0019598ff9343
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-10-11 17:31:05 +0300

    new admin controller

--- Project_Fitness.Server/DTO/AddGymDTO.cs
+++ Project_Fitness.Server/DTO/AddGymDTO.cs
@@ -2,5 +2,18 @@
 {
     public class AddGymDTO
     {
+        public string GymName { get; set; };
+
+        public string? GymImage { get; set; }
+
+        public string? GymDescription { get; set; }
+
+        public string? GymLocation { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public string? StartTime { get; set; }
+
+        public string? EndTime { get; set; }
     }
 }
