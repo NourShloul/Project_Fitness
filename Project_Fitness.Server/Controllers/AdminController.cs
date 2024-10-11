427460568ca81c184135f86bef5f1f7847be17dd
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-10-11 17:30:47 +0300

    new admin Controller

---
+++ Project_Fitness.Server/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project_Fitness.Server.Models;
+
+namespace Project_Fitness.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public AdminController(MyDbContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpPost("AddNewGym")]
+        public async Task<IActionResult> AddNewGym() {
+            var NewGym = new Gym {
+            GymName=
+            };
+            return Ok();
+        }
+
+    }
+}
