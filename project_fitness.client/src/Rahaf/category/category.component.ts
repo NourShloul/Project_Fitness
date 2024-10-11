34d3166e6ff5ab9b8d2c035f2fc0aab9da695663
roe-sh <92922959+roe-sh@users.noreply.github.com>
2024-10-11 16:02:38 +0300

    the front od products and categories

---
+++ project_fitness.client/src/Rahaf/category/category.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CategoriesService } from '../categories.service';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css'
+})
+export class CategoryComponent {
+
+
+  constructor(private categoryService: CategoriesService, private router: Router) { }
+
+ngOnInit(): void {
+  this.categoryService.getCategory().subscribe(
+    (data) => {
+      this.categories = data;
+    },
+    (error) => {
+      console.error('Error fetching categories:', error);
+    }
+  );
+}
+
+viewCategoryProducts(categoryId: number): void {
+
+  this.router.navigate(['/products/category', categoryId]);
+}
+}
