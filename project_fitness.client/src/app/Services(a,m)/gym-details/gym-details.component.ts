f12cebb0bee47c47b083c706c23d6c5ecd422b17
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-10-12 17:31:19 +0300

    new changes

--- project_fitness.client/src/app/Services(a,m)/gym-details/gym-details.component.ts
+++ project_fitness.client/src/app/Services(a,m)/gym-details/gym-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { URLService } from '../../url/url.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-gym-details',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './gym-details.component.css'
 })
 export class GymDetailsComponent {
+  parameter: any
+  array: any
+  ngOnInit() {
+    this.parameter = this._rout.snapshot.paramMap.get('id');
+    this.getDetails(this.parameter)
+    console.log(this.DetailsArray, "details")
+  }
+  constructor(private _ser: URLService, private _rout: ActivatedRoute) { }
 
+  DetailsArray: any
+  getDetails(id: any) {
+    this._ser.getGymDeyails(id).subscribe((data:any) => {
+
+      this.DetailsArray = data
+    })
+
+  }
 }
