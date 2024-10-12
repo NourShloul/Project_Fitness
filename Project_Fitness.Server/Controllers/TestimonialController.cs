43007ec9d904cd015d6305a7e8b0e58f0bdb3cea
ahmadqadomi-1 <qadomia95@gmail.com>
2024-10-13 00:36:03 +0300

    done

--- Project_Fitness.Server/Controllers/TestimonialController.cs
+++ Project_Fitness.Server/Controllers/TestimonialController.cs
@@ -17,6 +17,7 @@ namespace Project_Fitness.Server.Controllers
             _db = db;
         }
 
+
         [HttpPost("AddTestimonial/{id}")]
         public IActionResult Addtestimonial(int id, [FromBody] AddtestimonialDTO addtestimonialDTO)
         {
@@ -44,21 +45,66 @@ namespace Project_Fitness.Server.Controllers
             return Ok();
         }
 
+
         [HttpGet("GetAllAcceptedTestimonial")]
         public IActionResult GetAllAcceptedTestimonial()
         {
             var testimonials = _db.Testimonials
-     .Include(c => c.User) 
-     .Where(d => d.IsAccept == true) 
-     .Select(c => new {
+            .Include(c => c.User) 
+            .Where(d => d.IsAccept == true) 
+            .Select(c => new {
          Username = c.User.UserName, 
          userimage=c.User.UserImage,
          TestimonialText = c.TestimonialMessege    
-     }).ToList();
-           
-
+        }).ToList();
             return Ok(testimonials);
+        }
 
+
+        [HttpPut("AcceptTestimonial/{id}")]
+        public IActionResult AcceptTestimonial(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("You can not use 0 or negative value for id");
+            }
+            var testimonial = _db.Testimonials.FirstOrDefault(u => u.TestimonialId == id);
+            if (testimonial == null)
+            {
+                return NotFound();
+            }
+            testimonial.IsAccept = true;
+            _db.Testimonials.Update(testimonial);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpGet("GetAllTestimonials")]
+        public IActionResult GetAllTestimonials()
+        {
+            var testimonials = _db.Testimonials.ToList();
+            
+            return Ok(testimonials);
+        }
+
+
+        [HttpDelete("DeleteTestimonial/{id}")]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("You can not use 0 or negative value for id");
+            }
+
+            var testmonial = _db.Testimonials.FirstOrDefault(u => u.TestimonialId == id);
+            if (testmonial == null)
+            {
+                return NotFound();
+            }
+            _db.Testimonials.Remove(testmonial);
+            _db.SaveChanges();
+            return Ok();
         }
     }
 }
