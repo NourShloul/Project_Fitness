838bc975f9969047220eee7171eebb03aa90356e
NourShloul <shloulnour31@gmail.com>
2024-10-13 09:41:08 +0300

    create profile controller

---
+++ Project_Fitness.Server/Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project_Fitness.Server.Models;
+
+namespace Project_Fitness.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        public ProfileController(MyDbContext db) { _db = db; }
+
+    }
+}
