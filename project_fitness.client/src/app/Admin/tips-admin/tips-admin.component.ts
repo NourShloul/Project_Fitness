13567763259c9f3e490cfebcf8bea3b97a247720
mustafa-almomani <almomani363@gmail.com>
2024-10-14 15:33:53 +0300

    gnd

--- project_fitness.client/src/app/Admin/tips-admin/tips-admin.component.ts
+++ project_fitness.client/src/app/Admin/tips-admin/tips-admin.component.ts
@@ -4,29 +4,44 @@ import { URLService } from '../../url/url.service';
 @Component({
   selector: 'app-tips-admin',
   templateUrl: './tips-admin.component.html',
-  styleUrl: './tips-admin.component.css'
+  styleUrls: ['./tips-admin.component.css']
 })
 export class TipsAdminComponent {
+
+  searchTerm: string = '';  // متغير للبحث
+  RecipeArray: any[] = [];  // جميع النصائح الأصلية
+  filteredTipsArray: any[] = [];  // النصائح المفلترة
+
+  constructor(private _ser: URLService) { }
+
   ngOnInit() {
-    this.GetAllTip()
+    this.GetAllTip();
   }
-  constructor(private _ser:
-    URLService) { }
 
-  RecipeArray: any
   GetAllTip() {
-
     this._ser.GetAllTips().subscribe((data) => {
-      this.RecipeArray = data
-      console.log(this.RecipeArray)
+      this.RecipeArray = data;
+      this.filteredTipsArray = this.RecipeArray;  // عرض جميع البيانات عند التحميل
+      console.log(this.RecipeArray);
+    });
+  }
 
-    })
+  filterTips() {
+    if (!this.searchTerm) {
+      // إذا لم يكن هناك نص في البحث، عرض جميع النصائح
+      this.filteredTipsArray = this.RecipeArray;
+    } else {
+      // تصفية النصائح بناءً على الاسم
+      this.filteredTipsArray = this.RecipeArray.filter(tip =>
+        tip.tipsName.toLowerCase().includes(this.searchTerm.toLowerCase())  // البحث في اسم النصائح
+      );
+    }
   }
 
   delettips(id: any) {
     this._ser.deletTips(id).subscribe(() => {
-      alert("Tips deleted successfully")
-      this.GetAllTip();
-    })
+      alert("Tips deleted successfully");
+      this.GetAllTip();  // تحديث القائمة بعد الحذف
+    });
   }
 }
